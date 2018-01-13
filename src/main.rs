ac2c1ab9ac135fe389c89713e40c13a3fe6b19ec
Daniël de Kok <me@danieldk.eu>
2018-01-13 14:27:01 +0100

    Basic SVG rendering using rsvg/Cairo/Gtk+.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+extern crate cairo;
 extern crate conllx;
 extern crate dot;
 extern crate getopts;
+extern crate gtk;
 extern crate petgraph;
+extern crate rsvg;
 extern crate stdinout;
 
 use std::env::args;
 use std::io::{Read, Write};
 use std::process::{self, Command, Stdio};
 
+use cairo::Context;
 use dot::render;
 use getopts::Options;
+use gtk::prelude::*;
+use gtk::DrawingArea;
+use rsvg::{Handle, HandleExt};
 use stdinout::{Input, OrExit};
 
 mod graph;
@@ -31,10 +38,18 @@ fn dot_to_svg(dot: &[u8]) -> String {
         .spawn()
         .or_exit("Couldn't spawn dot", 1);
 
-    process.stdin.unwrap().write_all(dot).or_exit("Cannot write to dot stdin", 1);
+    process
+        .stdin
+        .unwrap()
+        .write_all(dot)
+        .or_exit("Cannot write to dot stdin", 1);
 
     let mut svg = String::new();
-    process.stdout.unwrap().read_to_string(&mut svg).or_exit("Cannot read dot stdout", 1);
+    process
+        .stdout
+        .unwrap()
+        .read_to_string(&mut svg)
+        .or_exit("Cannot read dot stdout", 1);
 
     svg
 }
@@ -67,20 +82,61 @@ fn main() {
     let input = Input::from(matches.free.get(0));
     let reader = conllx::Reader::new(input.buf_read().or_exit("Cannot open input for reading", 1));
 
-    for sentence in reader {
-        let sentence = sentence.or_exit("Cannot read sentence", 1);
+    let sentence = reader.into_iter().next().unwrap();
+    let sentence = sentence.or_exit("Cannot read sentence", 1);
 
-        let graph = sentence_to_graph(&sentence, false);
-        //let simplified_graph = graph.map(|_, n| n.token.form(), |_, e| e.unwrap());
+    let graph = sentence_to_graph(&sentence, false);
 
-        //println!("{:?}", Dot::with_config(&simplified_graph, &[]));
+    let mut dot = Vec::new();
+    render(&graph, &mut dot).or_exit("Error writing dot output", 1);
+    let svg = dot_to_svg(&dot);
 
-        let mut dot = Vec::new();
-        render(&graph, &mut dot).or_exit("Error writing dot output", 1);
-        let svg = dot_to_svg(&dot);
+    gtk::init().or_exit("Failed to initialize GTK", 1);
 
-        println!("{}", svg);
+    // FIXME: should not terminate the viewer.
+    let handle = Handle::new_from_data(svg.as_bytes()).or_exit("Error parsing SVG", 1);
+    let svg_dims = handle.get_dimensions();
 
-        return;
-    }
+    // SVG drawing from rsvg-rs example.
+    drawable(500, 500, move |drawing_area, cr| {
+        let (da_width, da_height) = (
+            drawing_area.get_allocated_width(),
+            drawing_area.get_allocated_height(),
+        );
+        let (svg_width, svg_height) = (svg_dims.width, svg_dims.height);
+        let (scale_x, scale_y) = (
+            da_width as f64 / svg_width as f64,
+            da_height as f64 / svg_height as f64,
+        );
+        let scale = if scale_x < scale_y { scale_x } else { scale_y };
+
+        cr.scale(scale, scale);
+
+        cr.paint_with_alpha(0.0);
+        handle.render_cairo(&cr);
+
+        Inhibit(false)
+    });
+
+    gtk::main();
+}
+
+pub fn drawable<F>(width: i32, height: i32, draw_fn: F)
+where
+    F: Fn(&DrawingArea, &Context) -> Inhibit + 'static,
+{
+    let window = gtk::Window::new(gtk::WindowType::Toplevel);
+    window.set_title("conllx-view");
+    let drawing_area = Box::new(DrawingArea::new)();
+
+    drawing_area.connect_draw(draw_fn);
+
+    window.set_default_size(width, height);
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+    window.add(&drawing_area);
+    window.show_all();
 }
